4a2dc95b0a582d9bdcbdf67a832b48352a7d7679
SLEEPlESS <rosas_u@ymail.com>
2022-02-04 11:40:51 -0800

    aded generate carusel function with api response info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
+var carouselImgEl = document.getElementById('free-to-play-carousel');
+
 
 //  async function searchGames(category) {
 
 //  let response = await fetch(`https://www.freetogame.com/api/games?platform=pc&category=${category}&sort-by=relavance`, {
 //  	"method": "GET",
 //  	"headers": {
-//  		// "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+ 		// "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
 //  		// "x-rapidapi-key": "7762a46affmsh847d232c8c0054bp190086jsnefdc4c528d13",
 //  		"Access-Control-Allow-Origin": "*"
 //  	}
@@ -33,19 +35,59 @@ $.ajax(settings).done(function (response) {
 	for(i = 0; i <= 9; i++){
 	// gets the game url object from the asrray in to a variable
 	const gameUrl = response[i].game_url;
-	console.log(gameUrl);
+	
 	// gets the thumb nail object from the array in to a var
 	const gameThumbnail = response[i].thumbnail;
 	console.log(gameThumbnail);
 	// passing the two variables to the freeToPlayCarousel function
-	freeToPlayCarousel(gameThumbnail,gameUrl);
+	generateCarouselEl(gameThumbnail,gameUrl, response[i], i);
 
 	}
 });
 
+function generateCarouselEl(gameThumbnail, gameUrl, response, index) {
+	var carouselItem = document.createElement('div');
+	carouselItem.classList.add('carousel-item');
+	//only the first slide is set to active class
+	if (index === 0) {
+	  carouselItem.classList.add('active');
+	}
+  
+	var imgItem = document.createElement('img');
+	imgItem.setAttribute("src", gameThumbnail);
+	imgItem.setAttribute("alt", response.title);
+	imgItem.setAttribute("class", "image-width")
+	console.log(response.title)
+	var anchor = document.createElement("a");
+	anchor.setAttribute("href",gameUrl );
+	imgItem.appendChild(anchor);
+	console.log(gameUrl);
 
-// function to create carusel with object information from response function
-function freeToPlayCarousel(gameThumbnail,gameUrl) {
-	// use the variables from previous function as normal to build carousel
+  
+	console.log("Image created ", imgItem, index);
+  
+  
+	var descriptionEL = document.createElement('div');
+	descriptionEL.setAttribute("class", "carousel-caption d-none d-md-block");
+	 
+  
+	var carouselCaption = document.createElement('h5');
+	carouselCaption.textContent = response.title;
+	
+  
+	var shortDescription = document.createElement('p');
+	shortDescription.textContent = response.short_description;
+  
+	//append to the div carousel-caption
+	descriptionEL.append(carouselCaption);
+	descriptionEL.append(shortDescription);
+  
+	//appending to img and div to carousel-item div 
+	carouselItem.append(imgItem);
+	carouselItem.append(descriptionEL);
+  
+	//Append Carousel cards to main div CarouselImg 
+	carouselImgEl.appendChild(carouselItem);
+  };
 
-}
+
