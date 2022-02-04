acb38b06554a1ed428522e274f6bda37c084db19
SLEEPlESS <rosas_u@ymail.com>
2022-02-03 23:42:03 -0800

    made variables from object and passed them to new function for carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,51 @@
 
-async function searchGames(category) {
+//  async function searchGames(category) {
 
-let response = await fetch(`https://www.freetogame.com/api/games?platform=pc&category=${category}&sort-by=relavance`, {
+//  let response = await fetch(`https://www.freetogame.com/api/games?platform=pc&category=${category}&sort-by=relavance`, {
+//  	"method": "GET",
+//  	"headers": {
+//  		// "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+//  		// "x-rapidapi-key": "7762a46affmsh847d232c8c0054bp190086jsnefdc4c528d13",
+//  		"Access-Control-Allow-Origin": "*"
+//  	}
+//  })
+
+//  let res = await response.json()
+// console.log(response)
+// }
+
+// searchGames("shooter"); 
+ 
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://free-to-play-games-database.p.rapidapi.com/api/games?platform=pc&category=shooter",
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
-		"x-rapidapi-key": "7762a46affmsh847d232c8c0054bp190086jsnefdc4c528d13",
-		"Access-Control-Allow-Origin": "*"
+		"x-rapidapi-key": "7762a46affmsh847d232c8c0054bp190086jsnefdc4c528d13"
 	}
-})
+};
+// response from game api in jquery
+$.ajax(settings).done(function (response) {
+	console.log(response);
+	// choses first ten array of objects
+	for(i = 0; i <= 9; i++){
+	// gets the game url object from the asrray in to a variable
+	const gameUrl = response[i].game_url;
+	console.log(gameUrl);
+	// gets the thumb nail object from the array in to a var
+	const gameThumbnail = response[i].thumbnail;
+	console.log(gameThumbnail);
+	// passing the two variables to the freeToPlayCarousel function
+	freeToPlayCarousel(gameThumbnail,gameUrl);
 
-// let res = await response.json()
-console.log(response)
+	}
+});
+
+
+// function to create carusel with object information from response function
+function freeToPlayCarousel(gameThumbnail,gameUrl) {
+	// use the variables from previous function as normal to build carousel
+
 }
-// fetch("https://free-to-play-games-database.p.rapidapi.com/api/games").then(function(response) {
-// 	console.log(response);
-// })
-searchGames("shooter");
