596879f86fa6b9b6376bdac83a227507ecf323ab
SLEEPlESS <rosas_u@ymail.com>
2022-02-05 23:11:45 -0800

    made new carusel display options by only keeping one active class

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,49 @@
 var carouselImgEl = document.getElementById('free-to-play-carousel');
 
- 
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://free-to-play-games-database.p.rapidapi.com/api/games?platform=pc&category=shooter",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
-		"x-rapidapi-key": "7762a46affmsh847d232c8c0054bp190086jsnefdc4c528d13"
-	}
-};
-// response from game api in jquery
-$.ajax(settings).done(function (response) {
-	// console.log(response);
-	// choses first ten array of objects
-	for(i = 0; i <= 9; i++){
-	// gets the game url object from the asrray in to a variable
-	const gameUrl = response[i].game_url;
+ function apiFreeToPlayRequest(dropDownSelection) {
+	console.log(dropDownSelection);
 	
-	// gets the thumb nail object from the array in to a var
-	const gameThumbnail = response[i].thumbnail;
-	// console.log(gameThumbnail);
-	// passing the two variables to the freeToPlayCarousel function
-	generateCarouselEl(gameThumbnail,gameUrl, response[i], i);
+	const settings = {
+		 "async": true,
+		 "crossDomain": true,
+		 "url": `https://free-to-play-games-database.p.rapidapi.com/api/games?category=${dropDownSelection}`,
+		 "method": "GET",
+		 "headers": {
+			 "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+			 "x-rapidapi-key": "7762a46affmsh847d232c8c0054bp190086jsnefdc4c528d13"
+		 }
+	 };
+	 console.log(settings.url);
+	 // response from game api in jquery
+	 $.ajax(settings).done(function (response) {
+		 console.log(response);
+		//  clear out the carousel items by targetting free-to-play-carousel 
+		   let carouselItems = document.querySelector(".carousel-item");
+		   console.log(carouselItems);
 
-	}
-});
+		// try to remove the active classthat is given to the firsat item so that they dont conflict
+		$(".carousel-item").removeClass("active");
+		 // choses first ten array of objects
+		 for (let i = 0; i < Math.min(response.length, 10); i++) {
+			 // gets the game url object from the array in to a variable
+			 const gameUrl = response[i].game_url;
+			 console.log(response[i]);
+
+			 // gets the thumb nail object from the array in to a var
+			 const gameThumbnail = response[i].thumbnail;
+			 // console.log(gameThumbnail);
+			 // passing the two variables to the freeToPlayCarousel function
+
+			 generateCarouselEl(gameThumbnail, gameUrl, response[i], i);
+
+		 }
+	 });
+
+ };
 
 function generateCarouselEl(gameThumbnail, gameUrl, response, index) {
+	
+	
 	var carouselItem = document.createElement('div');
 	carouselItem.classList.add('carousel-item');
 	//only the first slide is set to active class
@@ -71,17 +86,26 @@ function generateCarouselEl(gameThumbnail, gameUrl, response, index) {
   
 	//Append Carousel cards to main div CarouselImg 
 	carouselImgEl.appendChild(carouselItem);
-  };
+};
 
 // function to handle drop doen form selection
 function dropDownSelection(){
 	console.log("dropdown menu was selected");
+	// get value from option to change the API endpoint
 	const dropDownSelection = $("#dropdown-form").find(":selected").val();
 	console.log(dropDownSelection);
-	// pass dropDown to the api request function
+	// pass dropDownSelection to the api request function
+	apiFreeToPlayRequest(dropDownSelection);
 
-	
+}
+
+function initDisplay() {
+	apiFreeToPlayRequest("shooter");
+
 }
 
 // used change so that just the options trigger the event and not just clicking on the drop down
-$('#dropdown-form').on('change', dropDownSelection)
+$('#dropdown-form').on('change', dropDownSelection) 
+
+initDisplay();
+
